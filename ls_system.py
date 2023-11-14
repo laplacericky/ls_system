0daec02377e13a7c4d902a6b890777b567f0be88
Laplace Ricky <wailun089@gmail.com>
2023-11-14 15:42:20 +0800

    add cmp mode add workdir folder update submodule lib_io

--- ls_system.py
+++ ls_system.py
@@ -15,19 +15,30 @@ def by_size():
     r = subprocess.run(['du', '-sh', *Path('.').iterdir()], stdout = subprocess.PIPE, check = True)
     return subprocess.run(['sort', '-h'], input = r.stdout, stdout = subprocess.PIPE, check = True).stdout.decode().splitlines()
 
+def compare(f1, f2):
+    assert not f1.is_symlink() and not f2.is_symlink()
+    if f1.is_file():
+        assert f2.is_file()
+    else:
+        assert f2.is_dir()
+    return subprocess.run(['git', 'diff', '--no-index', '--name-status', f1, f2], stdout = subprocess.PIPE, text = True).stdout.splitlines()
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('mode', choices = ['largest','smallest','newest','oldest','search'] )
-    parser.add_argument('target', nargs='?')
+    parser.add_argument('mode', choices = ['largest','smallest','newest','oldest','search','cmp'] )
+    parser.add_argument('target', nargs='*')
     parser.add_argument('-n', '--head', type=int, default = 10)
     parser.add_argument('-a','--all',action='store_true')
     args = parser.parse_args()
 
     match args.mode:
         case 'largest'|'smallest'|'newest'|'oldest':
-            assert args.target is None
+            assert not args.target
         case 'search':
-            assert args.target is not None
+            assert len(args.target) == 1
+        case 'cmp':
+            assert len(args.target) == 2
+            args.target = [Path(t) for t in args.target]
 
     match args.mode:
         case 'largest'|'smallest':
@@ -35,14 +46,16 @@ def main():
         case 'newest'|'oldest':
             result = by_date()
         case 'search':
-            result = search_name(args.target)
+            result = search_name(*args.target)
+        case 'cmp':
+            result = compare(*args.target)
 
     match args.mode:
         case 'largest'|'oldest':
             start = -1
             end = -args.head-1
             step = -1
-        case 'smallest'|'newest'|'search':
+        case 'smallest'|'newest'|'search'|'cmp':
             start = 0
             end = args.head
             step = 1
